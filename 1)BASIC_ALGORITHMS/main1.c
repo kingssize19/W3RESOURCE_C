706e30fe0b0700b81f2db4e5f5511b30e3ed4295
Hayati Kılıç <83482374+kingssize19@users.noreply.github.com>
2024-07-18 19:13:38 +0300

    Update main1.c

--- 1)BASIC_ALGORITHMS/main1.c
+++ 1)BASIC_ALGORITHMS/main1.c
@@ -1,5 +1,6 @@
-### Write a C program to compute the sum of the two input values. If the two values are the same, then return triple their sum.
+#### Write a C program to compute the sum of the two input values. If the two values are the same, then return triple their sum.
 
+```C
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -17,3 +18,4 @@ int main(int argc, char *argv[]) {
 	
 	return 0;
 }
+```
