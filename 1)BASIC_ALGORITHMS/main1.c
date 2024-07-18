c4b84a17c70b3e71efe406bcaa775d5afe489dc6
Hayati Kılıç <83482374+kingssize19@users.noreply.github.com>
2024-07-18 19:12:05 +0300

    Create main1.c

---
+++ 1)BASIC_ALGORITHMS/main1.c
@@ -0,0 +1,20 @@
+//Write a C program to compute the sum of the two input values. If the two values are the same, then return triple their sum.
+'''C
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	int ival1, ival2;
+	
+	printf("Please enter the two numbers : ");
+	scanf("%d%d", &ival1, &ival2);
+	
+	int result = ival1 + ival2;
+	
+	printf("%d + %d = %d\n", ival1, ival2, result);
+	
+	return 0;
+}
+'''
